2497a1629cf2b911fd6dc96c9f3c842060868eba
Ming-Yi Lin <acdo2002@gmail.com>
2023-05-23 14:46:20 +0800

    finish step 2 in SET_HISTOGRAMCONFIG.test.ts

--- src/test/SET_HISTOGRAMCONFIG.test.ts
+++ src/test/SET_HISTOGRAMCONFIG.test.ts
@@ -2,6 +2,7 @@ import { CARTA } from "carta-protobuf";
 import { checkConnection, Stream} from './myClient';
 import { MessageController } from "./MessageController";
 import config from "./config.json";
+import { take } from 'rxjs/operators';
 
 let testServerUrl = config.serverURL0;
 let testSubdirectory = config.path.QA;
@@ -11,7 +12,6 @@ let openFileTimeout = config.timeout.openFile;
 
 interface AssertItem {
     openFile: CARTA.IOpenFile;
-    setCursor: CARTA.ISetCursor;
     addTilesRequire: CARTA.IAddRequiredTiles;
     precisionDigits: number;
     setHistogramRequirements: CARTA.ISetHistogramRequirements[];
@@ -27,10 +27,6 @@ let assertItem: AssertItem = {
         fileId: 0,
         hdu: "0",
         renderMode: CARTA.RenderMode.RASTER,
-    },
-    setCursor: {
-        fileId: 0,
-        point: { x: 1, y: 1 },
     },
     addTilesRequire:
     {
@@ -94,6 +90,7 @@ let assertItem: AssertItem = {
             stokes: 1,
             config: 
             {
+                bounds:{min: -1, max: 0.1376738934777677},
                 fixedBounds: true, 
                 fixedNumBins: true, 
                 numBins: 2
@@ -106,6 +103,47 @@ let assertItem: AssertItem = {
                 mean: -0.0000026881257400475778,
                 numBins: 2,
                 stdDev: 0.0142919735819505,
+            }
+        },
+        {
+            channel: 3,
+            progress: 1,
+            regionId: -1,
+            stokes: 1,
+            config: 
+            {
+                bounds:{min: -1, max: 0.1376738934777677},
+                fixedBounds: true, 
+                fixedNumBins: true, 
+                numBins: 2
+            },
+            histograms: 
+            {
+                binWidth: 0.5688369274139404,
+                bins: [0, 736705],
+                firstBinCenter: -0.7155815362930298,
+                mean: 0.000002222024835655828,
+                numBins: 2,
+                stdDev: 0.002096762523721388,
+            }
+        },
+        {
+            channel: 3,
+            progress: 1,
+            regionId: -1,
+            stokes: 1,
+            config: 
+            {
+                bounds:{},
+                numBins: -1
+            },
+            histograms: 
+            {
+                binWidth: 0.00003724902853718959,
+                firstBinCenter: -0.016793109476566315,
+                mean: 0.000002222024835655828,
+                numBins: 1049,
+                stdDev: 0.002096762523721388,
             }
         }
     ]
@@ -138,8 +176,8 @@ describe("Testing set region ICD message to all annotation RegionTypes and expor
 
                 msgController.addRequiredTiles(assertItem.addTilesRequire);
                 let RasterTileDataResponse = await Stream(CARTA.RasterTileData,3);
-                msgController.setCursor(assertItem.setCursor.fileId, assertItem.setCursor.point.x, assertItem.setCursor.point.y);
-                let SpatialProfileDataResponse = await Stream(CARTA.SpatialProfileData,1);
+            
+                //click Histogram widget
                 msgController.setHistogramRequirements(assertItem.setHistogramRequirements[0]);
                 let RegionHistogramDataReponse = await Stream(CARTA.RegionHistogramData,1);
                 expect(RegionHistogramDataReponse[0].progress).toEqual(assertItem.ResponseRegionHistogramData[0].progress);
@@ -153,21 +191,76 @@ describe("Testing set region ICD message to all annotation RegionTypes and expor
                 expect(RegionHistogramDataReponse[0].histograms.stdDev).toBeCloseTo(assertItem.ResponseRegionHistogramData[0].histograms.stdDev, assertItem.precisionDigits);
             }, openFileTimeout);
 
-            // let RegionHistogramData: CARTA.IRegionHistogramData;
-            // test(`(Case 1) set Bounds and NumBins, and then receive the first RegionHistogramData:`, async () => {
-            //     console.log(assertItem.setHistogramRequirements[0])
-            //     msgController.setHistogramRequirements(assertItem.setHistogramRequirements[0]);
-            //     RegionHistogramData = await Stream(CARTA.RegionHistogramData,1);
-            //     console.log(RegionHistogramData);
-            //     // msgController.setHistogramRequirements(assertItem.setHistogramRequirements[0]);
-            // });
+            let RegionHistogramData: CARTA.IRegionHistogramData;
+            test(`(Case 1) set Bounds and NumBins, and then receive the first RegionHistogramData:`, async () => {
+                msgController.setHistogramRequirements(assertItem.setHistogramRequirements[1]);
+                RegionHistogramData = await Stream(CARTA.RegionHistogramData,1);
+            });
 
-            // test(`(Case 1) Check the correction of the first RegionHistogramData`, () => {
-            //     // expect(RegionHistogramData[0].progress).toEqual(assertItem.ResponseRegionHistogramData[0].progress);
-            //     // expect(RegionHistogramData[0].regionId).toEqual(assertItem.ResponseRegionHistogramData[0].regionId);
-            //     // expect(RegionHistogramData[0].stokes).toEqual(assertItem.ResponseRegionHistogramData[0].stokes);
-            //     // expect(RegionHistogramData[0].config.fixedBounds).toEqual(assertItem.ResponseRegionHistogramData[0].config.fixedBounds);
-            // });
+            test(`(Case 1) Check the correction of the first RegionHistogramData`, () => {
+                expect(RegionHistogramData[0].progress).toEqual(assertItem.ResponseRegionHistogramData[1].progress);
+                expect(RegionHistogramData[0].regionId).toEqual(assertItem.ResponseRegionHistogramData[1].regionId);
+                expect(RegionHistogramData[0].stokes).toEqual(assertItem.ResponseRegionHistogramData[1].stokes);
+                expect(RegionHistogramData[0].config.bounds).toEqual(assertItem.ResponseRegionHistogramData[1].config.bounds);
+                expect(RegionHistogramData[0].config.fixedBounds).toEqual(assertItem.ResponseRegionHistogramData[1].config.fixedBounds);
+                expect(RegionHistogramData[0].config.fixedNumBins).toEqual(assertItem.ResponseRegionHistogramData[1].config.fixedNumBins);
+                expect(RegionHistogramData[0].config.numBins).toEqual(assertItem.ResponseRegionHistogramData[1].config.numBins);
+                expect(RegionHistogramData[0].histograms.binWidth).toBeCloseTo(assertItem.ResponseRegionHistogramData[1].histograms.binWidth, assertItem.precisionDigits);
+                expect(RegionHistogramData[0].histograms.firstBinCenter).toBeCloseTo(assertItem.ResponseRegionHistogramData[1].histograms.firstBinCenter, assertItem.precisionDigits);
+                expect(RegionHistogramData[0].histograms.mean).toBeCloseTo(assertItem.ResponseRegionHistogramData[1].histograms.mean, assertItem.precisionDigits);
+                expect(RegionHistogramData[0].histograms.numBins).toBeCloseTo(assertItem.ResponseRegionHistogramData[1].histograms.numBins, assertItem.precisionDigits);
+                expect(RegionHistogramData[0].histograms.stdDev).toBeCloseTo(assertItem.ResponseRegionHistogramData[1].histograms.stdDev, assertItem.precisionDigits);
+                expect(RegionHistogramData[0].histograms.bins).toEqual(assertItem.ResponseRegionHistogramData[1].histograms.bins);
+            });
+
+            let RegionHistogramDataResponse: CARTA.RegionHistogramData[] = [];
+            let RasterTileDataResponse: any = [];
+            test(`(Case 2) Set image channel, and then receive the RegionHistogramData * 2 + RasterTileData * 20 + RasterTileSync * 2:`, async () => {
+                msgController.setChannels(assertItem.setImageChannel);
+                msgController.histogramStream.pipe(take(2)).subscribe({
+                    next: (data) => {
+                        RegionHistogramDataResponse.push(data)
+                    }
+                })
+                RasterTileDataResponse = await Stream(CARTA.RasterTileData,22);
+            });
+
+            test(`(Case 2) Check the correction of RegionHistogramData * 2 + RasterTileData * 20 + RasterTileSync * 2:`, () => {
+                let firstRegionHistogramData = RegionHistogramDataResponse.filter(data => data.histograms.numBins == 2);
+                expect(firstRegionHistogramData[0].channel).toEqual(assertItem.ResponseRegionHistogramData[2].channel);
+                expect(firstRegionHistogramData[0].progress).toEqual(assertItem.ResponseRegionHistogramData[2].progress);
+                expect(firstRegionHistogramData[0].regionId).toEqual(assertItem.ResponseRegionHistogramData[2].regionId);
+                expect(firstRegionHistogramData[0].stokes).toEqual(assertItem.ResponseRegionHistogramData[2].stokes);
+                expect(firstRegionHistogramData[0].config.bounds).toEqual(assertItem.ResponseRegionHistogramData[2].config.bounds);
+                expect(firstRegionHistogramData[0].config.fixedBounds).toEqual(assertItem.ResponseRegionHistogramData[2].config.fixedBounds);
+                expect(firstRegionHistogramData[0].config.fixedNumBins).toEqual(assertItem.ResponseRegionHistogramData[2].config.fixedNumBins);
+                expect(firstRegionHistogramData[0].config.numBins).toEqual(assertItem.ResponseRegionHistogramData[2].config.numBins);
+                expect(firstRegionHistogramData[0].histograms.binWidth).toBeCloseTo(assertItem.ResponseRegionHistogramData[2].histograms.binWidth, assertItem.precisionDigits);
+                expect(firstRegionHistogramData[0].histograms.firstBinCenter).toBeCloseTo(assertItem.ResponseRegionHistogramData[2].histograms.firstBinCenter, assertItem.precisionDigits);
+                expect(firstRegionHistogramData[0].histograms.mean).toBeCloseTo(assertItem.ResponseRegionHistogramData[2].histograms.mean, assertItem.precisionDigits);
+                expect(firstRegionHistogramData[0].histograms.numBins).toBeCloseTo(assertItem.ResponseRegionHistogramData[2].histograms.numBins, assertItem.precisionDigits);
+                expect(firstRegionHistogramData[0].histograms.stdDev).toBeCloseTo(assertItem.ResponseRegionHistogramData[2].histograms.stdDev, assertItem.precisionDigits);
+                expect(firstRegionHistogramData[0].histograms.bins).toEqual(assertItem.ResponseRegionHistogramData[2].histograms.bins);
+
+                let secondRegionHistogramData = RegionHistogramDataResponse.filter(data => data.histograms.numBins == 1049);
+                expect(secondRegionHistogramData[0].channel).toEqual(assertItem.ResponseRegionHistogramData[3].channel);
+                expect(secondRegionHistogramData[0].progress).toEqual(assertItem.ResponseRegionHistogramData[3].progress);
+                expect(secondRegionHistogramData[0].regionId).toEqual(assertItem.ResponseRegionHistogramData[3].regionId);
+                expect(secondRegionHistogramData[0].stokes).toEqual(assertItem.ResponseRegionHistogramData[3].stokes);
+                expect(secondRegionHistogramData[0].config.bounds).toEqual(assertItem.ResponseRegionHistogramData[3].config.bounds);
+                expect(secondRegionHistogramData[0].config.numBins).toEqual(assertItem.ResponseRegionHistogramData[3].config.numBins);
+                expect(secondRegionHistogramData[0].histograms.binWidth).toBeCloseTo(assertItem.ResponseRegionHistogramData[3].histograms.binWidth, assertItem.precisionDigits);
+                expect(secondRegionHistogramData[0].histograms.firstBinCenter).toBeCloseTo(assertItem.ResponseRegionHistogramData[3].histograms.firstBinCenter, assertItem.precisionDigits);
+                expect(secondRegionHistogramData[0].histograms.mean).toBeCloseTo(assertItem.ResponseRegionHistogramData[3].histograms.mean, assertItem.precisionDigits);
+                expect(secondRegionHistogramData[0].histograms.numBins).toBeCloseTo(assertItem.ResponseRegionHistogramData[3].histograms.numBins, assertItem.precisionDigits);
+                expect(secondRegionHistogramData[0].histograms.stdDev).toBeCloseTo(assertItem.ResponseRegionHistogramData[3].histograms.stdDev, assertItem.precisionDigits);
+                expect(secondRegionHistogramData[0].histograms.bins.length).toEqual(assertItem.ResponseRegionHistogramData[3].histograms.numBins);
+
+                RasterTileDataResponse.map((data) => {
+                    expect(data.channel).toEqual(assertItem.setImageChannel.channel);
+                    expect(data.stokes).toEqual(assertItem.setImageChannel.stokes);
+                })
+            });
         });
 
         afterAll(() => msgController.closeConnection());
