c3e86f4996068fb048496d21873ae80a64ac43d6
Ming-Yi Lin <acdo2002@gmail.com>
2023-10-26 10:59:51 +0800

    finish PERF_LOAD_IMAGE_CASA.test.ts and PERF_LOAD_IMAGE_HDF5.test.ts

---
+++ src/performance/PERF_LOAD_IMAGE_CASA.test.ts
@@ -0,0 +1,93 @@
+import { CARTA } from "carta-protobuf";
+import { checkConnection, Stream} from './myClient';
+import { MessageController } from "./MessageController";
+import config from "./config.json";
+
+let testServerUrl: string = config.serverURL0;
+let testSubdirectory: string = config.path.performance;
+let connectTimeout: number = config.timeout.connection;
+let openFileTimeout: number = config.performance.openFile;
+let readFileTimeout: number = config.timeout.readFile;
+
+interface AssertItem {
+    registerViewer: CARTA.IRegisterViewer;
+    filelist: CARTA.IFileListRequest;
+    fileOpen: CARTA.IOpenFile[];
+    initTilesReq: CARTA.IAddRequiredTiles;
+    initSetCursor: CARTA.ISetCursor;
+    initSpatialRequirements: CARTA.ISetSpatialRequirements;
+}
+let assertItem: AssertItem = {
+    registerViewer: {
+        sessionId: 0,
+        clientFeatureFlags: 5,
+    },
+    filelist: { directory: testSubdirectory },
+    fileOpen: [
+        {
+            directory: testSubdirectory + "/cube_B",
+            file: "cube_B_09600_z00100.image",
+            hdu: "0",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+    ],
+    initTilesReq: {
+        fileId: 0,
+        compressionQuality: 11,
+        compressionType: CARTA.CompressionType.ZFP,
+        tiles: [33558529, 33558528, 33554433, 33554432, 33562625, 33558530, 33562624, 33554434, 33562626],
+    },
+    initSetCursor: {
+        fileId: 0,
+        point: { x: 1, y: 1 },
+    },
+    initSpatialRequirements:
+    {
+        fileId: 0,
+        regionId: 0,
+        spatialProfiles: [{coordinate: "x", mip: 1}, {coordinate: "y", mip: 1}],
+    },
+}
+
+let basepath: string;
+describe("PERF_LOAD_IMAGE",()=>{
+    const msgController = MessageController.Instance;
+    describe(`Register a session`, () => {
+        beforeAll(async ()=> {
+            await msgController.connect(testServerUrl);
+        }, connectTimeout);
+
+        checkConnection();
+
+        test(`Get basepath and modify the directory path`, async () => {
+            let fileListResponse = await msgController.getFileList("$BASE",0);
+            basepath = fileListResponse.directory;
+            assertItem.fileOpen[0].directory = basepath + "/" + assertItem.fileOpen[0].directory;
+        });
+
+        describe(`Initialization: open the image`, () => {
+            test(`(Step 1)"${assertItem.fileOpen[0].file}" OPEN_FILE_ACK and REGION_HISTOGRAM_DATA should arrive within ${openFileTimeout} ms`, async() => {
+                msgController.closeFile(-1);
+                msgController.closeFile(0);
+                let OpenFileResponse = await msgController.loadFile(assertItem.fileOpen[0]);
+                expect(OpenFileResponse.success).toEqual(true);
+                let RegionHistrogramDataResponse = await Stream(CARTA.RegionHistogramData,1);
+            }, openFileTimeout);
+
+            test(`(Step 1)"${assertItem.fileOpen[0].file}" SetImageChannels & SetCursor responses should arrive within ${readFileTimeout} ms`, async () => {
+                msgController.addRequiredTiles(assertItem.initTilesReq);
+                let RasterTileDataResponse = await Stream(CARTA.RasterTileData,assertItem.initTilesReq.tiles.length + 2);
+
+                msgController.setCursor(assertItem.initSetCursor.fileId, assertItem.initSetCursor.point.x, assertItem.initSetCursor.point.y);
+                let SpatialProfileDataResponse1 = await Stream(CARTA.SpatialProfileData,1);
+
+                msgController.setSpatialRequirements(assertItem.initSpatialRequirements);
+                let SpatialProfileDataResponse2 = await Stream(CARTA.SpatialProfileData,1);
+
+                expect(RasterTileDataResponse.length).toEqual(assertItem.initTilesReq.tiles.length + 2);
+            }, openFileTimeout);
+
+        });
+    });
+});
