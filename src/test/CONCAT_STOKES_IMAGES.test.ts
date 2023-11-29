9b40903c290d14754d8f4d14e932414c91db0671
Ming-Yi Lin <acdo2002@gmail.com>
2023-11-29 14:34:54 +0800

    finish CONCAT_STOKES_IMAGES.test.ts

--- src/test/CONCAT_STOKES_IMAGES.test.ts
+++ src/test/CONCAT_STOKES_IMAGES.test.ts
@@ -24,7 +24,9 @@ interface AssertItem {
     ConcatReqQU: CARTA.IConcatStokesFiles;
     RegionHistogramDataResponseQU: CARTA.IRegionHistogramData;
     ConcatReqIQU: CARTA.IConcatStokesFiles;
+    RegionHistogramDataResponseIQU: CARTA.IRegionHistogramData;
     ConcatReqQUV: CARTA.IConcatStokesFiles;
+    RegionHistogramDataResponseQUV: CARTA.IRegionHistogramData;
     ConcatResponse: ConcatStokesFilesAckExt[];
     precisionDigits: number;
 };
@@ -182,6 +184,20 @@ let assertItem: AssertItem = {
             },
         ],
     },
+    RegionHistogramDataResponseIQU: {
+        progress: 1,
+        regionId: -1,
+        config: {
+            numBins: -1
+        },
+        histograms: {
+            binWidth: 0.004779201466590166,
+            firstBinCenter: -0.11032065749168396,
+            numBins: 256,
+            stdDev: 0.05368401551544911,
+            mean: 0.0014072911570091893
+        }
+    },
     ConcatReqQUV: {
         fileId: 0,
         renderMode: 0,
@@ -205,6 +221,20 @@ let assertItem: AssertItem = {
                 polarizationType: 2
             },
         ],
+    },
+    RegionHistogramDataResponseQUV: {
+        progress: 1,
+        regionId: -1,
+        config: {
+            numBins: -1
+        },
+        histograms: {
+            binWidth: 0.00016267175669781864,
+            firstBinCenter: -0.018377140164375305,
+            numBins: 256,
+            stdDev: 0.0038693415380174558,
+            mean: -0.00003742659352908538
+        }
     },
     ConcatResponse:[
     {
@@ -467,6 +497,118 @@ describe("CONCAT_STOKES_IMAGES test: concatenate different stokes images into si
             expect(ConcatStokesResponse.openFileAck.fileInfoExtended.width).toEqual(assertItem.ConcatResponse[2].openFileAck.fileInfoExtended.width);
             expect(ConcatStokesResponse.openFileAck.fileInfoExtended.height).toEqual(assertItem.ConcatResponse[2].openFileAck.fileInfoExtended.height);
             expect(ConcatStokesResponse.openFileAck.fileInfoExtended.depth).toEqual(assertItem.ConcatResponse[2].openFileAck.fileInfoExtended.depth);
+        })
+    });
+
+    describe(`Case 4: Combine I, Q & U |`,() => {
+        test(`(Step 1) Assert FileListRequest |`, async()=>{
+            let FileListResponse = await msgController.getFileList(assertItem.filelist.directory, 0);
+            expect(FileListResponse.success).toEqual(true);
+        });
+
+        let FileInfoResponse: any = [];
+        let inputIndex = [0,1,2];
+        inputIndex.map((input,index) => {
+            test(`FILE_INFO_RESPONSE-${index+1} should arrive within ${openFileTimeout} ms" | `, async () => {
+                FileInfoResponse = await msgController.getFileInfo(testSubdirectory, assertItem.fileInfoReq[input].file, assertItem.fileInfoReq[input].hdu)
+                expect(FileInfoResponse.success).toEqual(true);
+            }, openFileTimeout);
+        });
+
+        let ConcatStokesResponse: any = [];
+        test(`(Step 2) Modify assert concatenate directory and request CONCAT_STOKES_FILES_ACK within ${concatStokeTimeout} ms | `,async()=>{
+            assertItem.ConcatReqIQU.stokesFiles.map((input,index) => {
+                assertItem.ConcatReqIQU.stokesFiles[index].directory = testSubdirectory;
+            });
+            msgController.closeFile(-1);
+            let regionHistogramDataArray = [];
+            let regionHistogramDataPromise = new Promise((resolve) => {
+                msgController.histogramStream.subscribe({
+                    next: (data) => {
+                        regionHistogramDataArray.push(data)
+                        resolve(regionHistogramDataArray)
+                    }
+                })
+            });
+            ConcatStokesResponse = await msgController.loadStokeFiles(assertItem.ConcatReqIQU.stokesFiles, assertItem.ConcatReqIQU.fileId, assertItem.ConcatReqIQU.renderMode);
+            let RegionHistogramData = await regionHistogramDataPromise;
+
+            expect(RegionHistogramData[0].regionId).toEqual(assertItem.RegionHistogramDataResponseIQU.regionId);
+            expect(RegionHistogramData[0].progress).toEqual(assertItem.RegionHistogramDataResponseIQU.progress);
+            expect(RegionHistogramData[0].config.numBins).toEqual(assertItem.RegionHistogramDataResponseIQU.config.numBins);
+            expect(RegionHistogramData[0].histograms.binWidth).toBeCloseTo(assertItem.RegionHistogramDataResponseIQU.histograms.binWidth, assertItem.precisionDigits);
+            expect(RegionHistogramData[0].histograms.firstBinCenter).toBeCloseTo(assertItem.RegionHistogramDataResponseIQU.histograms.firstBinCenter, assertItem.precisionDigits);
+            expect(RegionHistogramData[0].histograms.mean).toBeCloseTo(assertItem.RegionHistogramDataResponseIQU.histograms.mean, assertItem.precisionDigits);
+            expect(RegionHistogramData[0].histograms.numBins).toEqual(assertItem.RegionHistogramDataResponseIQU.histograms.numBins);
+            expect(RegionHistogramData[0].histograms.stdDev).toBeCloseTo(assertItem.RegionHistogramDataResponseIQU.histograms.stdDev, assertItem.precisionDigits); 
+        }, concatStokeTimeout);
+
+        test(`(Step 3) Check CONCAT_STOKES_FILES_ACK response | `,()=>{
+            expect(ConcatStokesResponse.success).toEqual(assertItem.ConcatResponse[3].success);
+            expect(ConcatStokesResponse.openFileAck.success).toEqual(assertItem.ConcatResponse[3].openFileAck.success);
+            expect(ConcatStokesResponse.openFileAck.beamTable.length).toEqual(assertItem.ConcatResponse[3].OpenFileAckBeamLength);
+            expect(ConcatStokesResponse.openFileAck.fileInfo.name).toEqual(assertItem.ConcatResponse[3].openFileAck.fileInfo.name);
+            expect(ConcatStokesResponse.openFileAck.fileInfoExtended.dimensions).toEqual(assertItem.ConcatResponse[3].openFileAck.fileInfoExtended.dimensions);
+            expect(ConcatStokesResponse.openFileAck.fileInfoExtended.stokes).toEqual(assertItem.ConcatResponse[3].openFileAck.fileInfoExtended.stokes);
+            expect(ConcatStokesResponse.openFileAck.fileInfoExtended.width).toEqual(assertItem.ConcatResponse[3].openFileAck.fileInfoExtended.width);
+            expect(ConcatStokesResponse.openFileAck.fileInfoExtended.height).toEqual(assertItem.ConcatResponse[3].openFileAck.fileInfoExtended.height);
+            expect(ConcatStokesResponse.openFileAck.fileInfoExtended.depth).toEqual(assertItem.ConcatResponse[3].openFileAck.fileInfoExtended.depth);
+        })
+    });
+
+    describe(`Case 5: Combine Q, U & V |`,() => {
+        test(`(Step 1) Assert FileListRequest |`, async() => {
+            let FileListResponse = await msgController.getFileList(assertItem.filelist.directory, 0);
+            expect(FileListResponse.success).toEqual(true);
+        });
+
+        let FileInfoResponse: any = [];
+        let inputIndex = [1,2,3];
+        inputIndex.map((input,index)=>{
+            test(`FILE_INFO_RESPONSE-${index+1} should arrive within ${openFileTimeout} ms" | `, async () => {
+                FileInfoResponse = await msgController.getFileInfo(testSubdirectory, assertItem.fileInfoReq[input].file, assertItem.fileInfoReq[input].hdu)
+                expect(FileInfoResponse.success).toEqual(true);
+            }, openFileTimeout);
+        });
+
+        let ConcatStokesResponse: any = [];
+        test(`(Step 2) Modify assert concatenate directory and request CONCAT_STOKES_FILES_ACK within ${concatStokeTimeout} ms | `,async()=>{
+            assertItem.ConcatReqQUV.stokesFiles.map((input,index) => {
+                assertItem.ConcatReqQUV.stokesFiles[index].directory = testSubdirectory;
+            });
+            msgController.closeFile(-1);
+            let regionHistogramDataArray = [];
+            let regionHistogramDataPromise = new Promise((resolve) => {
+                msgController.histogramStream.subscribe({
+                    next: (data) => {
+                        regionHistogramDataArray.push(data)
+                        resolve(regionHistogramDataArray)
+                    }
+                })
+            });
+            ConcatStokesResponse = await msgController.loadStokeFiles(assertItem.ConcatReqQUV.stokesFiles, assertItem.ConcatReqQUV.fileId, assertItem.ConcatReqQUV.renderMode);
+            let RegionHistogramData = await regionHistogramDataPromise;
+
+            expect(RegionHistogramData[0].regionId).toEqual(assertItem.RegionHistogramDataResponseQUV.regionId);
+            expect(RegionHistogramData[0].progress).toEqual(assertItem.RegionHistogramDataResponseQUV.progress);
+            expect(RegionHistogramData[0].config.numBins).toEqual(assertItem.RegionHistogramDataResponseQUV.config.numBins);
+            expect(RegionHistogramData[0].histograms.binWidth).toBeCloseTo(assertItem.RegionHistogramDataResponseQUV.histograms.binWidth, assertItem.precisionDigits);
+            expect(RegionHistogramData[0].histograms.firstBinCenter).toBeCloseTo(assertItem.RegionHistogramDataResponseQUV.histograms.firstBinCenter, assertItem.precisionDigits);
+            expect(RegionHistogramData[0].histograms.mean).toBeCloseTo(assertItem.RegionHistogramDataResponseQUV.histograms.mean, assertItem.precisionDigits);
+            expect(RegionHistogramData[0].histograms.numBins).toEqual(assertItem.RegionHistogramDataResponseQUV.histograms.numBins);
+            expect(RegionHistogramData[0].histograms.stdDev).toBeCloseTo(assertItem.RegionHistogramDataResponseQUV.histograms.stdDev, assertItem.precisionDigits); 
+        }, concatStokeTimeout);
+
+        test(`(Step 3) Check CONCAT_STOKES_FILES_ACK response | `,()=>{
+            expect(ConcatStokesResponse.success).toEqual(assertItem.ConcatResponse[4].success);
+            expect(ConcatStokesResponse.openFileAck.success).toEqual(assertItem.ConcatResponse[4].openFileAck.success);
+            expect(ConcatStokesResponse.openFileAck.beamTable.length).toEqual(assertItem.ConcatResponse[4].OpenFileAckBeamLength);
+            expect(ConcatStokesResponse.openFileAck.fileInfo.name).toEqual(assertItem.ConcatResponse[4].openFileAck.fileInfo.name);
+            expect(ConcatStokesResponse.openFileAck.fileInfoExtended.dimensions).toEqual(assertItem.ConcatResponse[4].openFileAck.fileInfoExtended.dimensions);
+            expect(ConcatStokesResponse.openFileAck.fileInfoExtended.stokes).toEqual(assertItem.ConcatResponse[4].openFileAck.fileInfoExtended.stokes);
+            expect(ConcatStokesResponse.openFileAck.fileInfoExtended.width).toEqual(assertItem.ConcatResponse[4].openFileAck.fileInfoExtended.width);
+            expect(ConcatStokesResponse.openFileAck.fileInfoExtended.height).toEqual(assertItem.ConcatResponse[4].openFileAck.fileInfoExtended.height);
+            expect(ConcatStokesResponse.openFileAck.fileInfoExtended.depth).toEqual(assertItem.ConcatResponse[4].openFileAck.fileInfoExtended.depth);
         })
     });
 
