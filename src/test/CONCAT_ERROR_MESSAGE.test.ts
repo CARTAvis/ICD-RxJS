6fa9996fef32632deababfd16f89d0e2d1a20032
Ming-Yi Lin <acdo2002@gmail.com>
2023-12-04 17:15:49 +0800

    move Q & axis-degeneracy Q to case 3 and finished it in  CONCAT_ERROR_MESSAGE.test.ts

--- src/test/CONCAT_ERROR_MESSAGE.test.ts
+++ src/test/CONCAT_ERROR_MESSAGE.test.ts
@@ -68,7 +68,7 @@ describe("PERF_ANIMATION_PLAYBACK",() => {
     let basepath: string;
 
     describe(`Case 1: Q & axis-degeneracy U, Image shape inconsistent`,()=>{
-        test(`(Step 1) Get basepath`, async()=>{
+        test(`(Step 1) Assert FileListRequest |`, async()=>{
             let fileListResponse = await msgController.getFileList("$BASE", 0);
             basepath = fileListResponse.directory;
         });
@@ -83,6 +83,26 @@ describe("PERF_ANIMATION_PLAYBACK",() => {
                 ConcatStokesResponse = await msgController.loadStokeFiles(assertItem.ConcatReqShape.stokesFiles, assertItem.ConcatReqShape.fileId, assertItem.ConcatReqShape.renderMode);
             } catch (err) {        
                 expect(err).toContain(assertItem.ConcatResponse[0]);
+            }
+        }, concatStokeTimeout);
+    });
+
+    describe(`Case 3: Q & axis-degeneracy Q, duplicated Stokes type`,() => {
+        test(`(Step 1) Assert FileListRequest |`, async()=>{
+            let fileListResponse = await msgController.getFileList("$BASE", 0);
+            basepath = fileListResponse.directory;
+        });
+
+        let ConcatStokesResponse: any = [];
+        test(`(Step 2) Modify assert concatenate directory and request CONCAT_STOKES_FILES_ACK within ${concatStokeTimeout} ms | `,async()=>{
+            assertItem.ConcatReqDeplicate.stokesFiles.map((input,index) => {
+                assertItem.ConcatReqDeplicate.stokesFiles[index].directory = testSubdirectory;
+            });
+            msgController.closeFile(-1);
+            try { 
+                ConcatStokesResponse = await msgController.loadStokeFiles(assertItem.ConcatReqDeplicate.stokesFiles, assertItem.ConcatReqDeplicate.fileId, assertItem.ConcatReqDeplicate.renderMode);
+            } catch (err) {        
+                expect(err).toContain(assertItem.ConcatResponse[1]);
             }
         }, concatStokeTimeout);
     });
