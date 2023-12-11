09e5f219b6c526b626b97c3e2211ccf179de86fd
Ming-Yi Lin <acdo2002@gmail.com>
2023-12-11 16:49:20 +0800

    finish CONTOUR_IMAGE_DATA.test.ts

--- src/test/CONTOUR_IMAGE_DATA.test.ts
+++ src/test/CONTOUR_IMAGE_DATA.test.ts
@@ -3,31 +3,21 @@ import config from "./config.json";
 import { checkConnection, Stream } from './myClient';
 import { MessageController } from "./MessageController";
 
-const ZstdCodec = require('zstd-codec').ZstdCodec;
 let testServerUrl: string = config.serverURL0;
 let testSubdirectory: string = config.path.QA;
 let connectTimeout: number = config.timeout.connection;
-let readTimeout: number = config.timeout.readFile;
+let openFileTimeout: number = config.timeout.openFile;
 
-interface ContourImageData extends CARTA.IContourImageData {
-    contourVertices?: number[];
-}
 interface AssertItem {
-    registerViewer: CARTA.IRegisterViewer;
     filelist: CARTA.IFileListRequest;
     openFile: CARTA.IOpenFile;
     addTilesReq: CARTA.IAddRequiredTiles;
     setCursor: CARTA.ISetCursor;
     setContour: CARTA.ISetContourParameters[];
-    contourImageData: ContourImageData[];
+    contourImageData: CARTA.IContourImageData[];
 };
 
 let assertItem: AssertItem = {
-    registerViewer: {
-        sessionId: 0,
-        apiKey: "",
-        clientFeatureFlags: 5,
-    },
     filelist: { directory: testSubdirectory },
     openFile: {
         directory: testSubdirectory,
@@ -95,24 +85,15 @@ let assertItem: AssertItem = {
                     level: 0.6,
                     decimationFactor: 4,
                     uncompressedCoordinatesSize: 104,
+                    rawCoordinates: new Uint8Array([40, 181,  47, 253,  32, 104, 245,   1,   0, 196,   2,  39,
+                    38, 255,   1,   0,   0, 255,   0, 255,   3,   1,   3,   0,
+                    0,   1,   1,   3,   1,   0,   3, 255,   1, 255,   0,   1,
+                    253, 255, 253,   0, 255, 255, 255, 255, 255, 253, 255, 253,
+                    255, 255, 255,   1,   0,   0,   0,   6,  32,  16, 218, 225,
+                    112,  50, 225,  25,  13, 141, 137,  77,  38,  38,  23]),
                 },
             ],
             progress: 1,
-            contourVertices: [
-                9.25, 9.00,
-                9.00, 9.25,
-                8.75, 10.00,
-                9.00, 10.75,
-                9.25, 11.00,
-                10.00, 11.25,
-                10.75, 11.00,
-                11.00, 10.75,
-                11.25, 10.00,
-                11.00, 9.25,
-                10.75, 9.00,
-                10.00, 8.75,
-                9.25, 9.00,
-            ],
         },
         {
             fileId: 0,
@@ -122,16 +103,13 @@ let assertItem: AssertItem = {
                     level: 0.6,
                     decimationFactor: 4,
                     uncompressedCoordinatesSize: 40,
+                    rawCoordinates: new Uint8Array([40, 181,  47, 253,  32,  40, 237,   0,   0, 168,
+                    36,  40,   4,   5,   0,   5, 251, 251, 252,   0,
+                    255, 255, 255, 252, 255, 255, 255,   4,   0,   0,
+                    0,   2,   0,  59, 194,  13, 160,   5]),
                 },
             ],
             progress: 1,
-            contourVertices: [
-                8.50, 9.50,
-                9.50, 10.75,
-                10.75, 9.50,
-                9.50, 8.50,
-                8.50, 9.50,
-            ],
         },
         {
             fileId: 0,
@@ -141,24 +119,121 @@ let assertItem: AssertItem = {
                     level: 0.85,
                     decimationFactor: 4,
                     uncompressedCoordinatesSize: 104,
+                    rawCoordinates: new Uint8Array([40, 181,  47, 253,  32, 104, 253,   1,   0, 212,   2,  40,
+                    38, 254,   2,   0,   0, 255,   0, 255,   2,   1,   2,   0,
+                    0,   2,   2,   2,   1,   0,   2, 255,   2, 254,   0, 255,
+                    1, 254, 255, 254,   0, 255, 255, 255, 254, 254, 254, 255,
+                    254, 255, 255, 255,   1,   0,   0,   0,   6,  32,  16, 218,
+                    225, 112,  50, 225, 237,   2, 141, 137,  77,  38,  38,  23]),
                 },
             ],
             progress: 1,
-            contourVertices: [
-                9.50, 9.00,
-                9.00, 9.50,
-                8.75, 10.00,
-                9.00, 10.50,
-                9.50, 11.00,
-                10.00, 11.25,
-                10.50, 11.00,
-                11.00, 10.50,
-                11.25, 10.00,
-                11.00, 9.50,
-                10.50, 9.00,
-                10.00, 8.75,
-                9.50, 9.00,
-            ],
         },
     ],
 };
+
+describe("CONTOUR_IMAGE_DATA: Testing if contour image data (vertices) are delivered correctly", () => {
+    const msgController = MessageController.Instance;
+    beforeAll(async ()=> {
+        await msgController.connect(testServerUrl);
+    }, connectTimeout);
+
+    checkConnection();
+
+    let basepath: string;
+    test(`Get basepath`, async () => {
+        let fileListResponse = await msgController.getFileList("$BASE", 0);
+        basepath = fileListResponse.directory;
+    });
+
+    describe(`Go to "${assertItem.filelist.directory}" folder`, () => {
+        let OpenFileResponse: CARTA.IOpenFileAck;
+        let regionHistogramData = [];
+        test(`(Step 1)"${assertItem.openFile.file}" OPEN_FILE_ACK and REGION_HISTOGRAM_DATA should arrive within ${openFileTimeout} ms`,async () => {
+            msgController.closeFile(-1);
+            assertItem.openFile.directory = basepath + "/" + assertItem.filelist.directory;
+            let regionHistogramDataPromise = new Promise((resolve)=>{
+                msgController.histogramStream.subscribe({
+                    next: (data) => {
+                        regionHistogramData.push(data)
+                        resolve(regionHistogramData)
+                    }
+                })
+            });
+            OpenFileResponse = await msgController.loadFile(assertItem.openFile);
+            let RegionHistogramData = await regionHistogramDataPromise;
+
+            expect(OpenFileResponse.success).toBe(true);
+            expect(OpenFileResponse.fileInfo.name).toEqual(assertItem.openFile.file);
+
+            msgController.addRequiredTiles(assertItem.addTilesReq);
+            let RasterTileDataResponse = await Stream(CARTA.RasterTileData,assertItem.addTilesReq.tiles.length + 2);
+            expect(RasterTileDataResponse.length).toEqual(assertItem.addTilesReq.tiles.length + 2);
+
+            msgController.setCursor(assertItem.setCursor.fileId, assertItem.setCursor.point.x, assertItem.setCursor.point.y);
+            let SpatialProfileDataResponse1 = await Stream(CARTA.SpatialProfileData, 1);
+            expect(SpatialProfileDataResponse1[0].x).toEqual(assertItem.setCursor.point.x);
+            expect(SpatialProfileDataResponse1[0].y).toEqual(assertItem.setCursor.point.y);
+        }, openFileTimeout);
+
+        assertItem.contourImageData.map((contour, index) => {
+            describe(`SET_CONTOUR_PARAMETERS${index} with SmoothingMode:"${CARTA.SmoothingMode[assertItem.setContour[index].smoothingMode]}"`, () => {
+                let ContourImageDataArray = [];
+                let ContourImageData : any;
+                test(`should return CONTOUR_IMAGE_DATA x1`, async () => {
+                    msgController.setContourParameters(assertItem.setContour[index]);
+                    let ContourImageDataPromise = new Promise((resolve)=>{
+                        msgController.contourStream.subscribe({
+                            next: (data) => {
+                                ContourImageDataArray.push(data)
+                                if (data.progress === 1) {resolve(ContourImageDataArray)}
+                            }
+                        });
+                    });
+                    ContourImageData = await ContourImageDataPromise;
+                });
+
+                test(`fileId = ${contour.fileId}`, () => {
+                    expect(ContourImageData[0].fileId).toEqual(contour.fileId);
+                });
+
+                test(`referenceFileId = ${contour.referenceFileId}`, () => {
+                    expect(ContourImageData[0].referenceFileId).toEqual(contour.referenceFileId);
+                });
+
+                test(`progress = 1`, () => {
+                    expect(ContourImageData[0].progress).toEqual(1);
+                });
+
+                test(`len(contourSet) = 1`, () => {
+                    expect(ContourImageData[0].contourSets.length).toEqual(1);
+                });
+
+                test(`contourSets[0].level = ${contour.contourSets[0].level}`, () => {
+                    expect(ContourImageData[0].contourSets[0].level).toEqual(contour.contourSets[0].level);
+                });
+
+                test(`contourSets[0].decimationFactor = ${contour.contourSets[0].decimationFactor}`, () => {
+                    expect(ContourImageData[0].contourSets[0].decimationFactor).toEqual(contour.contourSets[0].decimationFactor);
+                });
+
+                test(`contourSets[0].uncompressedCoordinatesSize = ${contour.contourSets[0].uncompressedCoordinatesSize}`, () => {
+                    expect(ContourImageData[0].contourSets[0].uncompressedCoordinatesSize).toEqual(contour.contourSets[0].uncompressedCoordinatesSize);
+                });
+
+                test(`number of rawCoordinates = ${contour.contourSets[0].rawCoordinates.length}`, () => {
+                    expect(ContourImageData[0].contourSets[0].rawCoordinates.length).toEqual(contour.contourSets[0].rawCoordinates.length);
+                });
+
+                test(`Check rawCoordinates value`, () => {
+                    for (let idx = 0; idx < contour.contourSets[0].rawCoordinates.length; idx++) {
+                        expect(ContourImageData[0].contourSets[0].rawCoordinates[idx]).toEqual(contour.contourSets[0].rawCoordinates[idx]);
+                    }
+                });
+
+            });
+        });
+    });
+
+    afterAll(() => msgController.closeConnection());
+});
