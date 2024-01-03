a81a31a272ef2031c2b5fe4c502b409a79418bcc
Ming-Yi Lin <acdo2002@gmail.com>
2024-01-03 20:10:44 +0800

    finish CURSOR_SPATIAL_PROFILE_NaN.test.ts

--- src/test/CURSOR_SPATIAL_PROFILE_NaN.test.ts
+++ src/test/CURSOR_SPATIAL_PROFILE_NaN.test.ts
@@ -12,10 +12,11 @@ let cursorTimeout = config.timeout.mouseEvent;
 
 interface ISpatialProfileDataExt extends CARTA.ISpatialProfileData {
     value?: number,
-    profileLength?: { x: number, y: number },
-    oddPoint?: {
-        x: { one: { idx: number, value: number }[], others?: number },
-        y: { one: { idx: number, value: number }[], others?: number },
+    profileEnd?: { x: number, y: number },
+    rawValuesFp32Length?: { x: number, y: number },
+    rawValuesPoint?: {
+        x: { index?: number[], values?: number[] },
+        y: { index?: number[], values?: number[] },
     }
 }
 interface AssertItem {
@@ -72,29 +73,25 @@ let assertItem: AssertItem = {
     spatialProfileData:
         [
             {
-                fileId: 0,
-                regionId: 0,
-                stokes: 0,
                 x: 314.0,
                 y: 393.0,
-                profileLength: { x: 640, y: 800 },
+                profileEnd: { x: 640, y: 800 },
+                rawValuesFp32Length: {x: 2560, y: 3200}, 
                 value: -0.004026404581964016,
-                oddPoint: {
-                    x: { one: [{ idx: 0, value: NaN }, { idx: 200, value: -0.0018224817467853427 }], others: null },
-                    y: { one: [{ idx: 799, value: NaN }, { idx: 400, value: 0.0019619895610958338 }], others: null },
+                rawValuesPoint: {
+                    x: { index: [500, 1000, 1500, 2000, 2500], values: [92, 142, 164, 171, 255] },
+                    y: { index: [500, 1000, 1500, 2000, 2500, 3000], values: [255, 56, 244, 246, 1, 255]},
                 }
             },
             {
-                fileId: 0,
-                regionId: 0,
-                stokes: 0,
                 x: 596.0,
                 y: 292.0,
-                profileLength: { x: 640, y: 800 },
+                profileEnd: { x: 640, y: 800 },
+                rawValuesFp32Length: {x: 2560, y: 3200}, 
                 value: NaN,
-                oddPoint: {
-                    x: { one: [], others: NaN },
-                    y: { one: [], others: NaN },
+                rawValuesPoint: {
+                    x: { index: [500, 1000, 1500, 2000, 2500], values: [90, 155, 28, 38, 255]},
+                    y: { index: [500, 1000, 1500, 2000, 2500, 3000], values: [255, 255, 255, 255, 255, 255]},
                 }
             },
         ],
@@ -146,6 +143,43 @@ describe("CURSOR_SPATIAL_PROFILE_NaN: Testing if full resolution cursor spatial 
 
         expect(RasterTileDataResponse.length).toEqual(3); //RasterTileSync: start & end + 1 Tile returned
     }, readFileTimeout);
+
+    assertItem.spatialProfileData.map((profileData, index) => {
+        describe(`set cursor on {${assertItem.setCursor[index].point.x}, ${assertItem.setCursor[index].point.y}}`, () => {
+            let SpatialProfileDataTemp: any;
+            test(`SPATIAL_PROFILE_DATA should arrive within ${cursorTimeout} ms`, async () => {
+                msgController.setCursor(assertItem.setCursor[index].fileId, assertItem.setCursor[index].point.x, assertItem.setCursor[index].point.y);
+                SpatialProfileDataTemp = await Stream(CARTA.SpatialProfileData,1);
+                SpatialProfileDataTemp = SpatialProfileDataTemp[0];
+            }, cursorTimeout);
+
+            test(`SPATIAL_PROFILE_DATA.value = ${profileData.value}`, () => {
+                if (isNaN(profileData.value)) {
+                    expect(SpatialProfileDataTemp.value).toEqual(NaN);
+                } else {
+                    expect(SpatialProfileDataTemp.value).toBeCloseTo(profileData.value, assertItem.precisionDigits);
+                }
+            });
+
+            test(`SPATIAL_PROFILE_DATA.x = ${profileData.x} and SPATIAL_PROFILE_DATA.y = ${profileData.y}`, () => {
+                expect(SpatialProfileDataTemp.x).toEqual(profileData.x);
+                expect(SpatialProfileDataTemp.y).toEqual(profileData.y);
+            });
+
+            test(`Check profiles.rawValuesFp32 value of x coordinate`, () => {
+                profileData.rawValuesPoint.x.index.map((f, index) => {
+                    expect(SpatialProfileDataTemp.profiles.find(f => f.coordinate === "x").rawValuesFp32[f]).toEqual(profileData.rawValuesPoint.x.values[index])
+                })
+            });
+
+            test(`Check profiles.rawValuesFp32 value of y coordinate`, () => {
+                profileData.rawValuesPoint.y.index.map((f, index) => {
+                    expect(SpatialProfileDataTemp.profiles.find(f => f.coordinate === "y").rawValuesFp32[f]).toEqual(profileData.rawValuesPoint.y.values[index])
+                })
+            });
+
+        });
+    });
 
     afterAll(() => msgController.closeConnection());
 });
