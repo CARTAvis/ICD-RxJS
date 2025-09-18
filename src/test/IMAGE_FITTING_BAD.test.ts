11f96b3a16f6f3cdeff14e4e049bc55fb0c26c49
pshnghng0318 <pshnghng@gmail.com>
2025-09-18 15:26:43 +0800

    update tests for ZFP in linux

--- src/test/IMAGE_FITTING_BAD.test.ts
+++ src/test/IMAGE_FITTING_BAD.test.ts
@@ -467,8 +467,8 @@ describe('IMAGE_FITTING_FITS test: Testing Image Fitting (with and without fov) 
                 }
             }
             if (platformOS === 'Linux') {
-                let Response = String(execSync('lsb_release -a', { encoding: 'utf-8' }));
-                isRhel8 = Response.includes('AlmaLinux 8') || Response.includes('Red Hat Enterprise Linux release 8');
+                let Response = String(execSync('cat /etc/redhat-release', {encoding:'utf-8'}));
+                isRhel8 = Response.includes('release 8');
             }
         }, connectTimeout);
 
