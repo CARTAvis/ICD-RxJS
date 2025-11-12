929c4cd1d29c706e1a191e3c200982ec58b3fdae
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2025-11-12 09:40:35 +0800

    Update the MessageController, myClient and config codes

--- src/performance/myClient.ts
+++ src/performance/myClient.ts
@@ -1,26 +1,26 @@
-import { CARTA } from "carta-protobuf";
-import {MessageController, ConnectionStatus} from "./MessageController";
+import { CARTA } from 'carta-protobuf';
+import { MessageController, ConnectionStatus } from './MessageController';
 import { take } from 'rxjs/operators';
 
 function checkConnection() {
     const msgController = MessageController.Instance;
-    test("check connection", () => {
-        expect(msgController.connectionStatus).toBe(ConnectionStatus.ACTIVE)
-    })
+    test('check connection', () => {
+        expect(msgController.connectionStatus).toBe(ConnectionStatus.ACTIVE);
+    });
 }
 
 function Stream(cartaType: any, InputNum?: number) {
-    return new Promise<any>((resolve,reject) => {
+    return new Promise<any>((resolve, reject) => {
         const msgController = MessageController.Instance;
-        let _count = 0;
-        switch(cartaType){
+        let count = 0;
+        switch (cartaType) {
             case CARTA.RegionHistogramData:
                 let RegionHistogramData: CARTA.RegionHistogramData[] = [];
                 let resRegionHistogramData = msgController.histogramStream.pipe(take(InputNum));
-                resRegionHistogramData.subscribe(data => {
+                resRegionHistogramData.subscribe((data) => {
                     RegionHistogramData.push(data);
-                    _count++;
-                    if (_count === InputNum){
+                    count++;
+                    if (count === InputNum) {
                         resolve(RegionHistogramData);
                     }
                 });
@@ -28,10 +28,10 @@ function Stream(cartaType: any, InputNum?: number) {
             case CARTA.SpatialProfileData:
                 let SpatialProfileData: CARTA.SpatialProfileData[] = [];
                 let resSpatialProfileData = msgController.spatialProfileStream.pipe(take(InputNum));
-                resSpatialProfileData.subscribe(data => {
+                resSpatialProfileData.subscribe((data) => {
                     SpatialProfileData.push(data);
-                    _count++;
-                    if (_count === InputNum){
+                    count++;
+                    if (count === InputNum) {
                         resolve(SpatialProfileData);
                     }
                 });
@@ -39,26 +39,37 @@ function Stream(cartaType: any, InputNum?: number) {
             case CARTA.RasterTileData:
                 let ack: any[] = [];
                 let ex1 = msgController.rasterSyncStream.pipe(take(2));
-                ex1.subscribe(data => {
-                    _count++;
+                ex1.subscribe((data) => {
+                    count++;
                     ack.push(data);
-                    if (data.endSync && _count === InputNum){
+                    if (data.endSync && count === InputNum) {
                         resolve(ack);
                     }
-                })
+                });
                 let ex2 = msgController.rasterTileStream.pipe(take(InputNum - 2));
-                ex2.subscribe(data => {
-                    _count++;
-                    ack.push(data)
-                })
+                ex2.subscribe((data) => {
+                    count++;
+                    ack.push(data);
+                });
+                break;
+            case CARTA.ListProgress:
+                let ListProgressData: any[] = [];
+                let resListProgressData = msgController.listProgressStream.pipe(take(InputNum));
+                resListProgressData.subscribe((data) => {
+                    ListProgressData.push(data);
+                    count++;
+                    if (count === InputNum) {
+                        resolve(ListProgressData);
+                    }
+                });
                 break;
             case CARTA.MomentProgress:
                 let MomentProgressData: any[] = [];
                 let resMomentProgressData = msgController.momentProgressStream.pipe(take(InputNum));
-                resMomentProgressData.subscribe(data => {
+                resMomentProgressData.subscribe((data) => {
                     MomentProgressData.push(data);
-                    _count++;
-                    if (_count === InputNum) {
+                    count++;
+                    if (count === InputNum) {
                         resolve(MomentProgressData);
                     }
                 });
@@ -66,83 +77,104 @@ function Stream(cartaType: any, InputNum?: number) {
             case CARTA.ErrorData:
                 let ErrorData: CARTA.IErrorData[] = [];
                 let resErrorData = msgController.errorStream.pipe(take(InputNum));
-                resErrorData.subscribe(data => {
+                resErrorData.subscribe((data) => {
                     ErrorData.push(data);
-                    _count++;
-                    if (_count === InputNum) {
+                    count++;
+                    if (count === InputNum) {
                         resolve(ErrorData);
                     }
                 });
                 break;
             case CARTA.ContourImageData:
-                let ContourImageData : CARTA.ContourImageData[] = [];
+                let ContourImageData: CARTA.ContourImageData[] = [];
                 let resContourImageData = msgController.contourStream.pipe(take(InputNum));
-                resContourImageData.subscribe(data => {
+                resContourImageData.subscribe((data) => {
                     ContourImageData.push(data);
-                    _count++;
-                    if (_count === InputNum) {
+                    count++;
+                    if (count === InputNum) {
                         resolve(ContourImageData);
                     }
-                })
+                });
                 break;
             case CARTA.ErrorData:
-                let ErrorStreamData : CARTA.ErrorData[] = [];
+                let ErrorStreamData: CARTA.ErrorData[] = [];
                 let resErrorStreamData = msgController.errorStream.pipe(take(InputNum));
-                resErrorStreamData.subscribe(data => {
+                resErrorStreamData.subscribe((data) => {
                     ErrorStreamData.push(data);
-                    _count++;
-                    if (_count === InputNum) {
+                    count++;
+                    if (count === InputNum) {
                         resolve(ErrorStreamData);
                     }
-                })
+                });
                 break;
             case CARTA.RegionStatsData:
-                let RegionStatsData : CARTA.RegionStatsData[] = [];
+                let RegionStatsData: CARTA.RegionStatsData[] = [];
                 let resRegionStatsData = msgController.statsStream.pipe(take(InputNum));
-                resRegionStatsData.subscribe(data => {
+                resRegionStatsData.subscribe((data) => {
                     RegionStatsData.push(data);
-                    _count++;
-                    if (_count === InputNum) {
+                    count++;
+                    if (count === InputNum) {
                         resolve(RegionStatsData);
                     }
-                })
+                });
                 break;
             case CARTA.SpectralProfileData:
-                let SpectralProfileData : any[] = [];
+                let SpectralProfileData: any[] = [];
                 let resSpectralProfileData = msgController.spectralProfileStream.subscribe({
                     next: (data) => {
                         SpectralProfileData.push(data);
                         if (data.progress === 1) {
-                            resolve(SpectralProfileData)
+                            resolve(SpectralProfileData);
                         }
-                    }
-                })
+                    },
+                });
                 break;
             case CARTA.CatalogFilterResponse:
-                let catalogStream : any [] = [];
-                let resCatalogStream = msgController.catalogStream.pipe(take(InputNum));
-                resCatalogStream.subscribe(data => {
-                    catalogStream.push(data);
-                    _count++;
-                    if (_count === InputNum) {
-                        resolve(catalogStream);
-                    }
-                })
+                let catalogStream: any[] = [];
+                let resCatalogStream = msgController.catalogStream.subscribe({
+                    next: (data) => {
+                        catalogStream.push(data);
+                        if (data.progress === 1) {
+                            resolve(catalogStream);
+                        }
+                    },
+                });
                 break;
             case CARTA.PvPreviewData:
-                let pvPreviewStream : any [] = [];
+                let pvPreviewStream: any[] = [];
                 let resPvPreviewStream = msgController.pvPreviewStream.pipe(take(InputNum));
-                resPvPreviewStream.subscribe(data => {
+                resPvPreviewStream.subscribe((data) => {
                     pvPreviewStream.push(data);
-                    _count++;
-                    if (_count === InputNum) {
+                    count++;
+                    if (count === InputNum) {
                         resolve(pvPreviewStream);
                     }
-                })
-                break;       
+                });
+                break;
         }
-    })
+    });
 }
 
+function ChannelMapStream(rasterTileDataLen: number, channels: number) {
+    return new Promise<any>((resolve, reject) => {
+        const msgController = MessageController.Instance;
+        const rasterTileMsgLen = (rasterTileDataLen + 2) * channels; // # of RasterTileData + 2 RasterTileSync per channel
+        let count = 0;
+        let rasterTileMsgs: any[] = [];
+        let rasterTileSyncStream = msgController.rasterSyncStream.pipe(take(2 * channels));
+        rasterTileSyncStream.subscribe((data) => {
+            count++;
+            rasterTileMsgs.push(data);
+            if (data.endSync && count === rasterTileMsgLen) {
+                resolve(rasterTileMsgs);
+            }
+        });
+        let rasterTileDataStream = msgController.rasterTileStream.pipe(take(rasterTileDataLen * channels));
+        rasterTileDataStream.subscribe((data) => {
+            count++;
+            rasterTileMsgs.push(data);
+        });
+    });
+}
 
-export { checkConnection , Stream };
+export { checkConnection, Stream, ChannelMapStream };
